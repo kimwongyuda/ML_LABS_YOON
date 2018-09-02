6e7913ae6e331f3b0e280f7d4edebbcdc3845ab6
kimwongyuda <35097915+kimwongyuda@users.noreply.github.com>
2018-09-02 15:10:11 +0900

    nlp, img

---
+++ img/img2vec_1.py
@@ -0,0 +1,14 @@
+"""
+해시:
+1. 해시 테이블: 딕셔너리, 객체
+2. 정해진 길이의 문자열
+"""
+
+import hashlib
+
+with open("tower.jpg", "rb") as file:
+    string = file.read()
+    m = hashlib.md5()
+    m.update(string)
+    result = m.digest()
+    print(result)
